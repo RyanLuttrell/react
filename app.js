ed1c85ffb1bb6070fedf280ec7f66abcfb7292e4
RyanLuttrell <ryan.m.luttrell@gmail.com>
2020-08-14 08:20:41 -0600

    Building out Header Player and Counter components

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-const title = 'My First React Element!';
-const desc = 'I just learned how to create a React node and render it to the DOM!';
-const myTitleID = 'main-title';
-const name = 'Ryan';
+const Header = () => {
+    return (
+        <header>
+            <h1>Scoreboard</h1>
+            <span className='stats'>Players: 1</span>
+        </header>
+    );
+}
 
-const header = (
-    <header>
-        <h1 id={myTitleID}>{name}'s First React Element</h1>
-        <p className="main-desc">{desc}</p>
-    </header>
-);
+const Player = () => {
+    return (
+        <div className='player'>
+            <span className='player-name'>
+                Ryan
+            </span>
+        </div>
+    );
+}
+
+const Counter = () => {
+    return (
+        <div className='counter'>
+            <button className='counter-action decrement'> - </button>
+            <span className='counter-score'>35</span>
+            <button className='counter-action increment'> + </button>
+        </div>
+    );
+}
 
 ReactDOM.render(
-    header,
+    <Player />,
     document.getElementById('root')
 );
