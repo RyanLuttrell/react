4778e5aa8ed033d8a14862903a89c373b5aa92fd
RyanLuttrell <ryan.m.luttrell@gmail.com>
2020-08-15 21:02:19 -0600

    Practice completed

--- practice-react/app.js
+++ practice-react/app.js
@@ -72,18 +72,18 @@ const planets = [
 // 1: Create a 'Planet' component that renders a planet card
 const Planet = (props) => {
   return (
-    <div className="card">
-    <div>
-      <img src={props.url} alt={props.name}/>
+    <div class="card">
+      <div>
+        <img src={props.url} alt={props.name} />
+      </div>
+      <h2>{props.name}</h2>
+      <p>{props.desc}</p>
+      <h3>Planet Profile</h3>
+      <ul>
+        <li><strong>Diameter:</strong> {props.diameter}</li>
+        <li><strong>Moons:</strong> {props.moons}</li>
+      </ul>
     </div>
-    <h2>{props.name}</h2>
-    <p>{props.desc}</p>
-    <h3>Planet Profile</h3>
-    <ul>
-      <li><strong>Diameter:</strong> {props.diameter}</li>
-      <li><strong>Moons:</strong> {props.moons}</li>
-    </ul>
-  </div>
   );
 }
 
@@ -108,6 +108,6 @@ const PlanetList = (props) => {
 
 // 3: Render the container component to the DOM
 ReactDOM.render(
-  <PlanetList planets={planets}/>,
+  <PlanetList planets={planets} />,
   document.getElementById('root')
-)
+);
