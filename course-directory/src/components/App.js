ea3b2b8cfe04e5d7c5256f99f6a4214f925ab195
RyanLuttrell <ryan.m.luttrell@gmail.com>
2020-08-20 08:19:21 -0600

    Add navigation functionality, routes, nested routes, redirects

--- course-directory/src/components/App.js
+++ course-directory/src/components/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Route
+} from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Header from './Header';
+import Teachers from './Teachers';
+import Courses from './Courses';
 
 const App = () => (
-  <div className="container">
+  <BrowserRouter>
+    <div className="container">
+      <Header />
+      <Route exact path="/" render={() => <Home />}/>
+      <Route path='/about' render={() => <About title='About' />}/>
+      <Route path='/teachers' component={Teachers}/>
+      <Route path='/courses' component={Courses}/>
+    </div>
+  </BrowserRouter>
 
-  </div>
 );
 
 export default App;
