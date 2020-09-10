ee5f85213ba2b7fe88a483aae1b38b0e33ab3dc7
RyanLuttrell <ryan.m.luttrell@gmail.com>
2020-09-10 12:57:01 -0600

    Add fetching and state

--- gif-search-hooks/src/Components/App.js
+++ gif-search-hooks/src/Components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import '../App.css';
 import axios from 'axios';
 
@@ -6,17 +6,33 @@ import SearchForm from './SearchForm';
 import GifList from './GifList';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState('cats');
+  const [isLoading, setIsLoading] = useState(true)
+
+  const performSearch = (value) => setQuery(value)
+
+  useEffect(() => {
+    axios(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=8T5swGCUDKBTYIN2gCMBkgviG1RZkwXY`)
+      .then(response => setData(response.data.data))
+      .catch(error => console.log("Error fetching and parsing data", error))
+      .finally(() => setIsLoading(false));
+  }, [query])
 
   return (
     <>
       <div className="main-header">
         <div className="inner">
           <h1 className="main-title">GifSearch</h1>
-          <SearchForm />
+          <SearchForm onSearch={performSearch}/>
         </div>
       </div>
       <div className="main-content">
-
+      {
+        isLoading
+          ? <p>Loading...</p>
+          : <GifList data={data}/>
+      }
       </div>
     </>
   );
