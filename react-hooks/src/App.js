b292e12a4e3bac70227209a87bbd373e113a5411
RyanLuttrell <ryan.m.luttrell@gmail.com>
2020-09-10 08:28:52 -0600

    Add first hooks

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [message, setMessage] = useState('Welcome!')
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome!</h1>
+        <h1>{message}</h1>      
+        <h1>{score}</h1>
+        <button onClick={() => setScore(prevScore => prevScore + 1)}>
+          Increase Score
+        </button>
+        <button onClick={() => setScore(prevScore => prevScore - 1)}>
+          Decrease Score
+        </button>
       </header>
     </div>
   );
